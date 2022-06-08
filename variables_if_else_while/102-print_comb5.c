f6909ecd5d35e4d794ba3bdc7b646eca2a0a9890
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-08 07:24:24 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num0;
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num0 = 0; num0 <= 9; num0++)
+	{
+		for (num1 = 0; num1 < 9; num1++)
+		{
+			for (num2 = 0; num2 <= 9; num2++)
+			{
+				for (num3 = 1; num3 <= 9; num3++)
+				{
+					putchar(num0 + '0');
+					putchar(num1 + '0');
+					putchar(' ');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
+					num4 = num0 - num1 - num2 - num3;
+					if (num4 != -17)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
