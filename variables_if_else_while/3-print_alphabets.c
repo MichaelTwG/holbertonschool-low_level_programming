025c73d52fc562bc7f055b744b6141b5c02f6c1e
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-07 17:27:21 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 97; letter <= 122; letter++)
+	{
+	putchar(letter);
+	}
+	for (letter = 65; letter <= 90; letter++)
+	{
+	putchar(letter);
+	}
+	putchar(10);
+	return (0);
+}
