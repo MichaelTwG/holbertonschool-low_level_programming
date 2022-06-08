fece6154ffe55a8d922384b45186a2c9fb0cd1d0
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-08 07:32:57 -0700

    C - Variables, if, else, while

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			for (num2 = 0; num2 <= 9; num2++)
 			{
-				for (num3 = 0; num3 <= 9; num3++)
+				for (num3 = 1; num3 <= 9; num3++)
 				{
 					putchar(num0 + '0');
 					putchar(num1 + '0');
