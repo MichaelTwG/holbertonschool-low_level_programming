5b0c59f752d6753d50f258e8ad620d8f7ae39a85
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-07 17:34:47 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+	puts(number);
+	}
+	putchar(10);
+	return (0);
+}
