b1ccf33ffe5deda854b22cc1192c742cbafe10fc
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-07 17:43:30 -0700

    C - Variables, if, else, while

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter >= 97; letter--)
+	{
+	putchar(letter);
+	}
+	putchar(10);
+	return (0);
+}
