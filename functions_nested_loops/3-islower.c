a9a4530522584c58c7dacf5e022318d0ff72ec51
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-09 10:55:03 -0700

    Functions in c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,21 +3,15 @@
  * _islower - checks for lowercase character
  * Return: 1 if c is lowercase. 0 otherwise
  * @c - charecter to check
- * @i - interation variable
  */
 int _islower(int c)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
+	if (c >= 97 && c <= 122)
 	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
