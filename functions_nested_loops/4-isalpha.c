e4ea9277f806d3cfe3a5ce7ae1fd68e6752b7f71
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-09 11:33:46 -0700

    Functions in c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "3-islower.c"
 /**
  * _islower - checks for lowercase character
  * Return: 1 if c is lowercase. 0 otherwise
@@ -7,8 +6,8 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90)
-		_islower(c);
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+		return (1);
 	else
 	{
 		return (0);
