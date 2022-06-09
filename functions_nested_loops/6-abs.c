f06952b1d929852068774422a258d904806dc7bc
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-09 12:30:00 -0700

    Functions in c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - display the absolute value of a number
+ * Return: return r
+ * @r: is a integer
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = r * -1;
+		return (r);
+	}
+	else
+	{
+		return (r);
+	}
+}
