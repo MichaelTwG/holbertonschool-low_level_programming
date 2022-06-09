fe86835ab77e2d21c32715582fa5b7826952cd30
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-09 12:38:45 -0700

    Functions in c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - display the las number of an int
+ * Return: num
+ * @r: integer
+ * @ld: last digit of r
+ */
+int print_last_digit(int r)
+{
+	int ld = r % 10;
+
+	return(ld);
+}
