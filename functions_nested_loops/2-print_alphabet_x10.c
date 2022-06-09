a4f3f86a2bf31fd39683ff4ad45f18ce609c7846
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-09 10:10:38 -0700

    Functions in c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,13 @@
  * Return: void
  * @i - is a number of times you can repeat the print_alphabet function
  */
-void print_alphabet_x10()
+void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while (i < 10)
 	{
 		print_alphabet();
 		i++;
 	}
 }
-
