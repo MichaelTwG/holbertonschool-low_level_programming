272d41e6f977c6c6fe3e28f0fec9081efd0e4f46
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-10 10:50:56 -0700

    Functions in c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,9 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-			printf("%d, ", n);
-	}
-	else
-	{
-		for (; n > 98; n--)
-			printf("%d, ", n);
+	for (; n < 98; n++)
+		printf("%d, ", n);
+	for (; n > 98; n--)
+		printf("%d, ", n);
 	printf("98\n");
-	}
 }
