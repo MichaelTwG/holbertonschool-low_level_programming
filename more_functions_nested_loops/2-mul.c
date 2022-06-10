674f944b701e297a73fcd3a1e030f8c9e11c9c9f
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-10 16:44:15 -0700

    C - More Functions

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul -
+ * Return:
+ * @a:
+ * @b:
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
+int mul(int, int);
 #include <stdio.h>
 #endif
