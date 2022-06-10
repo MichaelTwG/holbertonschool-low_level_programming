5105a7b3ac20efe024eeca926ac82c659162832f
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-10 10:23:47 -0700

    Functions in c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,10 +14,14 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n--;
 		}
-		if (n < 98)
+		else if (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
+		}
+		else
+		{
+			printf("%d, ", n);
 		}
 	}
 }
