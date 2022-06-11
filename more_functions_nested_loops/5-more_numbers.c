0a2716afa9226e5bbfcc3a3ba14c76671144d82e
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-10 20:15:47 -0700

    C - More Functions

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - display 10 times the numbers from 0 to 14
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar((j / 10) + 48);
+				_putchar((j % 10) + 48);
+			}
+			else
+			{
+				_putchar(j + 48);
+			}
+		}
+		_putchar(10);
+	}
+}
