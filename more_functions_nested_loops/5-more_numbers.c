b5aa05016b22209095f5c2f94b89ff303533bb74
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-10 20:54:59 -0700

    C - More Functions

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j ++)
 		{
-			if (j >= 10)
+			if (j > 9)
 			{
-				_putchar((j / 10) + 48);
-				_putchar((j % 10) + 48);
+				_putchar(49);
 			}
-			else
-			{
-				_putchar(j + 48);
-			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar(10);
 	}
