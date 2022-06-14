b85837a944d53cd721691b531fc54d64681f36e4
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-14 10:03:41 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- * 
- *
+ * _strlen - size of an string
+ * @s: is a string
  */
 int _strlen(char *s)
 {
-	for (i=0; *s[i] != '\0'; i++)
+	int i;
+
+	for (i=0; *s != '\0'; i++)
 	{
 	}
 	return i;
