30cc72473a1121747cdaf70a94d998fec32daf8e
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-16 15:46:50 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,15 @@
  * _strncat - concatenates n bytes of src to dest
  * @src: is a source
  * @dest: is the destiny
+ * @n: number of bytes to display
+ * Return: a pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int l;
 	int l_2;
 	int i;
+	int l_total;
 
 	for (l = 0; dest[l] != '\0'; l++)
 	{
@@ -16,7 +19,8 @@ char *_strncat(char *dest, char *src, int n)
 	for (l_2 = 0; src[l_2] != '\0'; l_2++)
 	{
 	}
-	for (i = 0; i < n && n <= l_2; i++)
+	l_total = l + l_2;
+	for (i = 0; i < n && l < l_total; i++)
 	{
 		dest[l] = src[i];
 		l++;
