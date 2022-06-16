b369063ebfaca1d575047cac6fd653f727a543bf
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-15 17:32:35 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@ void puts2(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{	
-		if ((i % 2) != 0)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
 		{
 			i++;
 		}
-		_putchar(str[i]);
 	}
 	_putchar(10);
 }
