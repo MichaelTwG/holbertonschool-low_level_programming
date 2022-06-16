7c98d04f33f6afb931a9f462671121be28f4d191
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-16 15:14:30 -0700

    C - Pointers, Arrays and Strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates n bytes of src to dest
+ * @src: is a source
+ * @dest: is the destiny
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l;
+	int l_2;
+	int i;
+
+	for (l = 0; dest[l] != '\0'; l++)
+	{
+	}
+	for (l_2 = 0; dest[l_2] != '\0'; l_2++)
+	{
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[l] = src[i];
+		l++;
+	}
+	return (dest);
+}
