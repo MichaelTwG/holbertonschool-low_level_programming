6a32af9975ffa9028a4e811e2fb544c4c28a0b36
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-15 17:29:51 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- *
- *
+ * puts2 - display in the screen every other character of a string
+ * @str: is a address to a string
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i]; i++)
-	{
-		if ((str[i] % 2) == 0)
+	for (i = 0; str[i] != '\0'; i++)
+	{	
+		if ((i % 2) != 0)
 		{
-			_putchar(str[i]);
+			i++;
 		}
+		_putchar(str[i]);
 	}
+	_putchar(10);
 }
