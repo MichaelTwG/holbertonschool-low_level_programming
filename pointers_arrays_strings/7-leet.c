4d6b818a8617a5f7d2aaa7899c0ab320fe743c7a
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-17 12:49:59 -0700

    C - Pointers, Arrays and Strings

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - write a function that encodes a string into 1337
+ * @dest: is a string
+ * Return: dest
+ */
+char *leet(char *dest)
+{
+	char str[] = "aeotl";
+	char str2[] = "43071";
+	int i;
+	int j;
+
+	for (i = 0; dest[i]; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (dest[i] == str[j] || (dest[i] - 32) == str[j])
+			{
+				dest[i] = str2[j];
+			}
+		}
+	}
+	return (dest);
+}
