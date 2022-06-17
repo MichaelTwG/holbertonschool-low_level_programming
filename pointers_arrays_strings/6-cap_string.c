97a57dfbc00a3f7e722cd2d14f83f9089470704c
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-17 09:58:15 -0700

    C - Pointers, Arrays and Strings

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char separator[13];
+
+	separator[0]=' ';
+	separator[1]='	';
+	separator[2]='\n';
+	separator[3]=',';
+	separator[4]=';';
+	separator[5]='.';
+	separator[6]='!';
+	separator[7]='?';
+	separator[8]='"';
+	separator[9]='(';
+	separator[10]=')';
+	separator[11]='{';
+	separator[12]='}';
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for(j = 0; j < 13; j++)
+		{
+			if (separator[j] == str[i] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
