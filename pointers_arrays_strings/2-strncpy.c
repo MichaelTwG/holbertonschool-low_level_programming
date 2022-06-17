000883fda53d55f5120cdfce9fae1666abcd6bb3
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-16 18:22:22 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,24 @@
 #include "main.h"
-#include "2-strlen.c"
 /**
  * _strncpy - copy the string dest, to the string src
  * @dest: the destiny string
  * @src: the source string
+ * @n: copy n bytes of src to dest
  * Return: dest
  *
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int l2 = _strlen(src);
 	int i;
 
-	for (i = 0; i < n && i <= l2; i++)
+	for (i = 0; i < n && src[i] != '\0' ; i++)
 	{
 		dest[i] = src[i];
+	}
+	
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
