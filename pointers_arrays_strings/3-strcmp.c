678e37dc33556c97068ea8101381ee29c306c085
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-16 19:18:37 -0700

    C - Pointers, Arrays and Strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
- *
- *
+ * _strcmp - compare string s1 with string s2
+ * @s1: is a string
+ * @s2: is a string
+ * Return: 0 if s1 and s2 is equial
+ * a value greather than 0 if s1 is greather than s2
+ * a value less than 0 if s2 is greather than s2
  */
 int _strcmp(char *s1, char *s2)
 {
