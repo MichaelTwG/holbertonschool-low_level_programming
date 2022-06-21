3a45a63316d18e79213ad7edc1b1cd2e7576ea7d
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-21 06:43:01 -0700

    Recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	y--;
+	return (x * _pow_recursion(x, y));
+}
