1e9a11b49df35c7feb150ce9d1515a07ac15c3cf
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-23 11:04:10 -0700

    Malloc - Free

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- *
- *
+ * _strdup - copy a string
+ * @str: is a sting :)
+ * Return: a pointer to a newly allocated space
+ * in memory, which contins a copy of a string str
  */
 char *_strdup(char *str)
 {
 	int i, j;
 	char *str2;
+
 	if (str == NULL)
 	{
 		return (NULL);
