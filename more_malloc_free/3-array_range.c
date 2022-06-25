1912fe2f16fad85507239b5610242710124ce5bd
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-25 12:30:23 -0700

    More | Malloc - Free

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	{
 		return(NULL);
 	}
-	arr = malloc(sizeof(int) * max);
+	arr = malloc(sizeof(int) * (max - min) + 4);
 	if (arr == NULL)
 		return (NULL);
 	for (; i <= max; i++)
