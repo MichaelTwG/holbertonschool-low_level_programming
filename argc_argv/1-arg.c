8a464392580c6fa2caae5aae51ab72b94ddefea3
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-26 18:51:33 -0700

    ARGC and ARGV in C

---
+++ argc_argv/1-arg.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print the name of the program
+ * @argc: is unused with (void)argc
+ * @argv: is used the position 0 containing the app name
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
