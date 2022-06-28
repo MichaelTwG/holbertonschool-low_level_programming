9640713368b9dbde4b41000cbd1e7cebc6160a21
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-28 11:20:00 -0700

    C - Structures - Typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
-#define DOG_H
-struct dog 
+#define DOG_Hi
+/**
+ * struct dog - name, age, owner of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner name
+ *
+ * Description: a dog is a dog
+ */
+struct dog
 {
 	char *name;
 	float age;
