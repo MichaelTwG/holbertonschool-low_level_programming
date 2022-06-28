0aa368908f6a2afa39f918b2a8895977074db344
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-28 09:45:15 -0700

    C - Preprocessor

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - display the name of the file it was compiled from
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
