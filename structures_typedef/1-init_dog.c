94e93d08f74090985986e5d109113630f6b8460a
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-28 13:19:54 -0700

    C - Structures - Typedef

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,11 +8,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == 0)
+	if (!)
 	{
 		return;
 	}
+	d->name = name;
 	d->age = age;
-	(*d).name = name;
-	(*d).owner = owner;
+	d->owner = owner;
 }
