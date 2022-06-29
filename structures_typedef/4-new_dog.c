5473797baf406fbb2b383a23edde8ba8a07ddd57
MichaelTwG <michaelgolenia1@gmail.com>
2022-06-29 08:48:43 -0700

    C - Structures - Typedef

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,11 +5,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *n_dog;
+	dog_t *Ndog;
+	char *cp_nam;
+	char *cp_own;
+	int i;
 
-	n_dog->age = age;
-	n_dog->name = name;
-	n_dog->owner = owner;
+	Ndog = malloc(sizeof(dog_t));
+	if (!Ndog || !name || !owner)
+		return (0);
 
-	return (n_dog);
+	cp_nam = malloc(sizeof(char) * (strlen(name) + 1));
+	if (!cp_nam)
+	{
+		free(Ndog);
+		return (0);
+	}
+	for (i = 0; name[i]; i++)
+	{
+		cp_nam[i] = name[i];
+	}
+	cp_nam[i] = 0;
+	cp_own = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (!cp_own)
+	{
+		free(cp_nam);
+		free(Ndog);
+		return (0);
+	}
+	for (i = 0; owner[i]; i++)
+	{
+		cp_own[i] = owner[i]
+	}
+	cp_own[i] = 0;
+
+	Ndog->name = cp_nam;
+	Ndog->age = age;
+	Ndog->owner = cp_own;
+
+	return(Ndog);
 }
