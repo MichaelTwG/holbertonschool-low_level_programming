2fbb70e7fafe57cb5502db746ef12ed75ff07faa
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-13 11:25:30 -0700

    betty error fixed

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  * list_len - print the number
- * @h - is a pointer to list_t
+ * @h: is a pointer to list_t
  * Return: the number of nodes in the list
  */
 size_t list_len(const list_t *h)
 {
 	int i = 0;
 	const list_t *actual = h;
+	
 	if (h)
 	{
 		for (; actual; actual = actual->next)
