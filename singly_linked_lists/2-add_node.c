c2c01678efcc517dafb37ac9cbe126ab700c92fa
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-13 13:07:24 -0700

    2-add_node.c added and lists.h update

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - add a node in the start
+ *
+ * @head: pointer to the first node
+ * @str: is the str to include in the struct
+ * Return: a pointer to the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len = strlen(str);
+	char *strcpy = strdup(str);
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->len = len;
+	new->str = strcpy;
+	new->next = (*head);
+	*head = new;
+	
+	return (new);
+}
