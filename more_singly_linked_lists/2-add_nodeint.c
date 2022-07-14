5304fe81fbebf8b8c5cc945efbb422d67109bc8a
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-14 08:35:01 -0700

    error in name of the repo

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,18 +5,18 @@
  * @n: is the number to put in the struct
  * Return: a pointer to a listint_t
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_node;
 
-	new = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-	if (!new)
+	if (!new_node)
 	{
 		return (NULL);
 	}
-	new->n = n;
-	new->next = (*head)
-	(*head) = new;
+	new_node->n = n;
+	new_node->next = (*head);
+	(*head) = new_node;
 	return (new_node);
 }
