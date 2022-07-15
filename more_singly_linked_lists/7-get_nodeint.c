04825e39f5a11033b15da2a70c138851b06729a7
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-15 13:43:29 -0700

    betty errors fixed

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 /**
- *
- *
+ * get_nodeint_at_index - return the addres of the nth node
+ * @head: the head of the list
+ * @index: the nth node to find
+ * Return: pointer to the struct
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
