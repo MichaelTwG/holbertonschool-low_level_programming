a9132660b682e3a987f2754d772525dcb519988c
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-15 13:25:57 -0700

    betty and memory errors fixed

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,18 +1,21 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * pop_listint - delete the head node of a list
+ * @head: the head of the list
+ * Return: return the value of the deleted head
  */
 int pop_listint(listint_t **head)
 {
 	int value;
+	listint_t *prev_head;
 
 	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
 	value = (*head)->n;
+	prev_head = (*head);
 	(*head) = (*head)->next;
+	free(prev_head);
 	return (value);
 }
