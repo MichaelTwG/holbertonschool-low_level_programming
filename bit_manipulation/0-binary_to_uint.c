bfb54e525b6dcd017e20abae641b3881c3fc5c55
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-19 05:42:44 -0700

    added 0-binary_to_uint.c file

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * raise - power a number x
+ * @x: base
+ * @y: exponent
+ * Return: unsigned int number
+ */
+unsigned int raise(unsigned int x, unsigned int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	y--;
+	return (x * raise(x, y));
+}
+/**
+ * binary_to_uint - converts a binary number to uint
+ * @b: binary
+ * Return: uint
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, i = (strlen(b) - 1), j = 0;
+
+	if (b != NULL)
+	{
+		for (; b[j]; i--, j++)
+		{
+			if (b[j] != '1' && b[j] != '0')
+			{
+				return (0);
+			}
+			if (b[j] == '1')
+			{
+				result += raise(2, i);
+			}
+		}
+	}
+	return (result);
+}
