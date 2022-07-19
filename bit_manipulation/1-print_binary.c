769def6407fbdaf7cc56f3d1fde929a154d81e4e
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-19 07:45:50 -0700

    added 1-print_binary.c f

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_binary - print a decimal as binary
+ * @n: is a decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
+}
