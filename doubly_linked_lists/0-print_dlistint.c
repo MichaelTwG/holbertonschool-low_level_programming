09e2cbeb40095ec671b2d6e97e2d5624a7bef7fe
MichaelTwG <michaelgolenia1@gmail.com>
2022-07-21 06:05:42 -0700

    betty errors fixed

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * print_dlistint - prit all elements in a dlistint_t
+ * @h: the header of dlistint_t
+ * Return: number of elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
@@ -13,8 +13,8 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	actual = h;
 	for (; actual; actual = actual->next, i++)
-		{
-			printf("%d\n", actual->n);
-		}
+	{
+		printf("%d\n", actual->n);
+	}
 	return (i);
 }
