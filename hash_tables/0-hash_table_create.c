c1a0f49709bc761d570f3764194b118af6f6e39c
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-21 09:35:49 -0300

    I don't know why valgrind says i have 3 malloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,26 +6,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t new_table;
+    hash_table_t *new_table;
 
-    new_table.size = size;
-    new_table.array = malloc(sizeof(hash_node_t *) * size);
-
-    if (!(new_table.array))
+    new_table = malloc(sizeof (hash_table_t *));
+    if (!new_table)
         return (NULL);
-    else
-        for (int i = 0; new_table.array[i]; i++)
-        {
-            new_table.array[i] = malloc(sizeof(hash_node_t *));
-            if (!(new_table.array[i]))
-            {
-                for (; i >= 0; i--)
-                {
-                    free(new_table.array[i]);
-                }
-                free(new_table.array);
-                return (NULL);
-            }
-        }
-        return (&new_table);
+
+    new_table->size = size;
+    new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+    if (!(new_table->array))
+    {
+        free(new_table);
+        return (NULL);
+    }
+
+    return (new_table);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef MAIN
 #define MAIN
-
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 /**
  * struct hash_node_s - Node of a hash table
