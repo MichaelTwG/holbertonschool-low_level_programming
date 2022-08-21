1d8696fbc3973ad516a304f812e0548f34cf679b
Michael Golenia <michaelgolenia1@gmail.com>
2022-08-21 10:46:44 -0300

    new_table = malloc(sizeof(hash_table_t))

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,20 +6,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
+	hash_table_t *new_table;
 
-    new_table = malloc(sizeof (hash_table_t *));
-    if (!new_table)
-        return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+	return (NULL);
 
-    new_table->size = size;
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
-    if (!(new_table->array))
-    {
-        free(new_table);
-        return (NULL);
-    }
+	if (!(new_table->array))
+	{
+		free(new_table);
+		return (NULL);
+	}
 
-    return (new_table);
+	return (new_table);
 }
